6b22a0405ff27d0535df0ddd2b7132e59fc9bc59
jungwon11082015 <jungwon11082015@gmail.com>
2021-12-06 23:55:22 +0900

    어바웃페이지 작업중
    어바웃페이지 작업중

--- pages/about.vue
+++ pages/about.vue
@@ -1,40 +1,35 @@
 <template>
-    <main>
-        <div class="about-container">
+    <div id="sections">
+        <section id="section0">
             <div class="about-title">
                 <div class="about-title-box">
                     <h1>We are a Digital</h1>
                     <h2>design Studio</h2>
                 </div>
-                <div class="gradient_box">
-                    <div class="design_box">
-                        <p class="design_arrow"></p>
-                    </div>
-                </div>
             </div>
-            <div class="about-text">
-                <div class="logo div_wrap">
-                    <div class="about-txt wow fadeInUp">
-                        <p class="txt">About us</p>
-                        <p class="left-txt">
-                            인더엑스는 다양한 경험속에서<br>
-                            완벽한 해답을 찾습니다.
-                        </p>
-                        <p class="right-txt">
-                            고객의 니즈와 트렌드를 반영하여 합리적인 가격으로 고퀄리티의 디자인을 제공하기 위해 눈에 보이지<br>
-                            않는 작은 부분까지 디테일하게 작업합니다. 우리의 혁신은 사용자들의 숨은 니즈를 찾는 것에서부터 시작<br>합니다. 인더엑스만의 정교한 리서치 설계와 프로세스 방법론을 통해 혁신적인 전략을 도출하고 내부<br>
-                            전문가 집단의 경험과 수행력을 더해 국내 최고의 UX/UI 디자인 서비스를 제공합니다.
-                        </p>
-                    </div>
-                    <div class="img-box wow fadeInUp">
-                        <p>
-                            <img src="imgs/studio/about_img_01.png" alt="컨텐츠이미지">
-                        </p>
-                        <span class="txt">Creative</span>
-                        <span class="bar"></span>
-                    </div>
+        </section>
+        <section id="section1">
+            <div class="inner">
+                <h2>About me</h2>
+                <h3>
+                    인더엑스는 다양한 경험속에서<br />
+                    완벽한 해답을 찾습니다.
+                </h3>
+                <div class="text-box">
+                    <span class="line"></span>
+                    <p>
+                        고객의 니즈와 트렌드를 반영하여 합리적인 가격으로 고퀄리티의 디자인을 제공하기 위해 눈에 보이지
+                        않는 작은 부분까지 디테일하게 작업합니다. 우리의 혁신은 사용자들의 숨은 니즈를 찾는 것에서부터 시작
+                        합니다. 인더엑스만의 정교한 리서치 설계와 프로세스 방법론을 통해 혁신적인 전략을 도출하고 내부
+                        전문가 집단의 경험과 수행력을 더해 국내 최고의 UX/UI 디자인 서비스를 제공합니다.
+                    </p>
                 </div>
+                <div class=""></div>
             </div>
+           
+        </section>
+
+        <section id="section2">
             <div class="img-cont-box wow fadeInUp">
                 <span class="bar"></span>
                 <img src="imgs/studio/about_img_02.png" alt="컨텐츠이미지">
@@ -48,51 +43,10 @@
                     <p class="txt">Passionate</p>
                 </div>
             </div>
-            <!-- start of :: service Sec-->
-            <article class="service-cont">
-                <div class="area">
-                    <p class="right-desc">
-                        프로덕트의 환경, 사용자의 경험, 디자이너의 감성을 바탕으로<br>
-                        그 어떠한 것보다 가치 있는 디자인을 표현합니다.
-                    </p>
-                    <div class="servi-txt-wrap">
-                        <p class="main-txt">
-                            디자인의 기획부터 개발까지<br>
-                            프로젝트의 A-Z 종합 디자인<br>
-                            서비스를 제공합니다.
-                        </p>
-                        <p class="right-txt">Our Services</p>
-                        <ul class=" wow fadeInDown">
-                            <li>
-                                <p><img src="imgs/studio/about_icon_01.png" alt="아이콘이미지"></p>
-                                <p class="desc">Strategy</p>
-                            </li>
-                            <li>
-                                <p><img src="imgs/studio/about_icon_02.png" alt="아이콘이미지"></p>
-                                <p class="desc">Branding</p>
-                            </li>
-                            <li>
-                                <p><img src="imgs/studio/about_icon_03.png" alt="아이콘이미지"></p>
-                                <p class="desc">UI / UX Design</p>
-                            </li>
-                            <li>
-                                <p><img src="imgs/studio/about_icon_04.png" alt="아이콘이미지"></p>
-                                <p class="desc">Develop</p>
-                            </li>
-                            
-                        </ul>
-                    </div>
-                </div>
-            </article>
 
-            <div class="main-box clients_box height">
-                <div class="area">
-                    <p class="tit">Our Partners</p>
-                    <p><img src="imgs/studio/partners_icon_bg.png" alt="기업로고이미지"></p>
-                </div>
-            </div>
-        </div>
-    </main>
+        </section>
+       
+    </div>
 </template> 
 
 <script>
@@ -106,16 +60,17 @@
   //사용법
       //@include toRem(width, 400);
   //
-  main{
-    background: #000;
+  #sections{
     @include toRem(padding-top, 240);
     @include toRem(padding-bottom, 130);
-        .about-container{
+    background: #000;
+    #section0{
             height: 100vh;
             background-size: cover;
             position: relative;
             width: 100%;
             overflow: hidden;
+            background: #000;
             .about-title{
                 position: absolute;
                 @include toRem(top, 10);
@@ -125,11 +80,11 @@
                 .about-title-box{
                     h1,
                     h2{
-                        @include toRem(font-size, 220);
+                        @include toRem(font-size, 180);
                         font-weight: bold;
                         letter-spacing: 3px;
                         margin-top: 2.5vw;
-                        @include toRem(line-height, 220);
+                        @include toRem(line-height, 180);
                         color: #fff;
                         font-family: 'Anton', sans-serif;
                     }
@@ -140,6 +95,50 @@
                     }
                 }
             }
+    }
+
+    #section1{
+        height: 100vh;
+        background-size: cover;
+        position: relative; 
+        width: 100%;
+        overflow: hidden;
+        background: #fff;
+        .inner{
+            padding:initRem( 100, 150, 0, 150  );
+            h2{
+                @include toRem(font-size, 20);
+                font-weight: 100;
+                font-family: 'Anton', sans-serif;
+             }
+             h3{
+                float: left;
+                padding:initRem( 26, 0, 0, 0  );
+                @include toRem(font-size, 46);
+                @include toRem(line-height, 72);
+                font-weight: bold;
+                font-family: 'Noto Sans KR';
+             }
+            .text-box{ 
+                float: left;
+                padding:initRem( 140, 0, 0, 150  );
+                @include toRem( width, 800 );     
+                span{ 
+                    display: inline-block;
+                    @include toRem( width, 100 );
+                    @include toRem( height, 1 );
+                    background: #000;
+                }
+                p{
+                    @include toRem(padding-top, 60);
+                    @include toRem(font-size, 18);
+                    @include toRem(line-height, 36);
+                    font-family: 'Noto Sans KR';
+                    
+                }
+            }
+            &:after{ content: ''; display: block; clear: both; }
+        }
     }
   }
 
