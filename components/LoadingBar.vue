29b68305b035d69e0661536b36b7ed4f25b05643
jungwon11082015 <jungwon11082015@gmail.com>
2021-12-23 00:43:29 +0900

    작업중
    작업중

--- components/LoadingBar.vue
+++ components/LoadingBar.vue
@@ -1,45 +1,79 @@
 <template>
   <div v-if="loading" class="loading-page">
-    <div class="counter">
-        <p>loading</p>
-        <h1>{{ num }}%</h1>
-        <span ref="line"></span>
+    <div class="loading-container">
+      <div class="loading-logo">
+        <em ref="loadingText">HeoPro-v</em>
+      </div>
     </div>
   </div>
 </template>
 <script>
+  import Mixin from '../plugins/MyMixin.js'
+  import {gsap, power2, power3, power4, Expo  } from "gsap";
   export default {
     data: () => ({
       loading: false,
       counter : 0,
-      num:0,
-    }),
  
-    methods: {
-      start() {
-        const timer = setInterval(() => {
-            this.num = this.counter;
-            this.counter++;
-            this.num++;
-            this.$refs.line.style.width = this.num + "%";
-
-            if( this.counter == 100 ){
-                console.log( "여기?" );
-                clearInterval( timer );
-            }
-        }, 50);
-
-        this.loading = true;
-      },
-      finish() {
-        this.counter = 0;
-        this.num = 0;
-        this.loading = false;
-      },
+    }),
+    created(){
+      if(!process.server){
+        console.log( "ddddddddddddddddddddddd" ); 
+      }else{
+        console.log( "2222")
+      }
     },
+    methods: {
+      // countStart() {
+      //   const timer = setInterval(() => {
+      //       this.counter++;      
+      //       if( this.counter == 100 ){
+      //           console.log( "??" );
+      //           clearInterval( timer );
+      //           // this.countFinish();
+      //       }
+      //   }, 50);
+
+      //   this.loading = true;
+      // },
+
+      // countFinish() {
+      //   this.counter = 0;
+      //   this.loading = false;
+      // },
+      logoAnimations(){
+         
+      }
+    },
+
+    mounted() {
+     
+      this.logoAnimations();
+      this.$nextTick(() => {
+        // this.countStart();
+      
+         
+      })
+  }
+
   }
 </script>
 
 <style lang="scss">
-  .loading-page{ position: fixed; top:0; left:0; width: 100vw; height: 100vw; background:#fff; z-index: 100;} 
+  .loading-page{ 
+    position: fixed; top:0; left:0; width: 100%; height: 100%; background:#fff; z-index: 100;
+    .loading-container{
+        display: table;
+        width: inherit;
+        height: inherit;
+        text-align: center;
+       
+        .loading-logo{
+          display: table-cell;
+           vertical-align: middle;
+          font-size:50px;
+          color: #000;
+        }
+      }
+    } 
 </style>
